71371c8d16d4559f796dcb241debe2cc78c293dc
Chris Hull <chris.f.hull@gmail.com>
2020-08-31 20:02:30 -0400

    adds awaiting approval page

--- eve-raiders-data/src/Dashboard/UnderDevelopment/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const UnderDevelopment = () => (
-  <div style={{ marginTop: 20 }}>
-    Feature currently under development. Come back later!
-  </div>
-);
-
-export default UnderDevelopment;
