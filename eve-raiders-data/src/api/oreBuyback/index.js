c4be9448f4e731df013b41a8242eec402bede31f
Chris Hull <chris.f.hull@gmail.com>
2020-09-02 17:24:35 -0400

    add admin page for ore buyback

--- eve-raiders-data/src/api/oreBuyback/index.js
+++ eve-raiders-data/src/api/oreBuyback/index.js
@@ -28,9 +28,24 @@ export const getUserOrders = async () => {
 
 export const getOrders = async () => {
   try {
-    const res = await instance.get("/api/administration​/orders​/buyback");
+    const res = await instance.get("/api/administration/orders/buyback");
 
-    return { success: res?.data?.status === "Success", data: res.data };
+    return res.data;
+  } catch (e) {
+    return {
+      error: e.response?.data?.message ||
+        e.message || ["An unknown error occured. Please try again."],
+    };
+  }
+};
+
+export const updateStatus = async ({ buyBackRequestId, status }) => {
+  try {
+    const res = await instance.put(
+      `/api/administration/buyback/${buyBackRequestId}/status/${status}`
+    );
+
+    return res.data;
   } catch (e) {
     return {
       error: e.response?.data?.message ||
