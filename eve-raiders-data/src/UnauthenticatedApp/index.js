4b9a79fabdc80d0d8adf134d2bc552c9a0aaf4c6
Chris Hull <chris.f.hull@gmail.com>
2020-09-09 15:51:52 -0400

    fix unauth page stylings

--- eve-raiders-data/src/UnauthenticatedApp/index.js
+++ eve-raiders-data/src/UnauthenticatedApp/index.js
@@ -1,8 +1,15 @@
 import React from "react";
 import Routes from "./Routes";
+import styles from "./UnauthenticatedApp.module.scss";
 
 const UnauthenticatedApp = () => {
-  return <Routes />;
+  return (
+    <div className={styles.unauthenticatedApp}>
+      <div className={styles.content}>
+        <Routes />
+      </div>
+    </div>
+  );
 };
 
 export default UnauthenticatedApp;
