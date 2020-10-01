c441688c09a30b2d38545b3a3e3a5e2dbd8d5820
Andrew Tsay <atsay714@gmail.com>
2020-10-01 13:06:35 -0700

    Added TBNR cause bigger team is more impressive. Win win

--- eve-raiders-data/src/AuthenticatedApp/NavBar/AboutModal/index.js
+++ eve-raiders-data/src/AuthenticatedApp/NavBar/AboutModal/index.js
@@ -5,12 +5,17 @@ import styles from "./AboutModal.module.scss";
 const AboutModal = ({ onClose }) => (
   <Modal onClose={onClose} hideSubmit>
     <span>This app was created by:</span>
+    <h2>Software Engineers</h2>
     <ul>
       <li className={styles.author}>Band#2556 (Hans Anmaicok)</li>
       <li className={styles.author}>xavi#9148 (Xaviair)</li>
       <li className={styles.author}>
         DoublyLinkedList#4995 (DoublyLinkedList)
       </li>
+    </ul>
+    <h2>Product Owners</h2>
+    <ul>
+      <li className={styles.author}>TBNRregs#6114 (TBNRregs)</li>
     </ul>
     <span>
       Feel free to reach out to us on
